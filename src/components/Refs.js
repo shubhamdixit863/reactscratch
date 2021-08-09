689a6ab3e47eb479e596c375d19e0d04c481c833
shubham dixit <shubhamdixit863@gmail.com>
2021-08-09 20:59:24 +0530

    talked about refs in react

--- src/components/Refs.js
+++ src/components/Refs.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
 
 
+
 // refs are an react utility to interact with traditional dom
 export default class Refs extends Component {
+    constructor()
+    {
+        super();
+
+        this.emailRef=React.createRef();
+        this.passRef=React.createRef();
+    }
+
+
+    handleChange=(event)=>{
+    if(event.keyCode===13)
+        {
+
+      //  this.emailRef.current.focus();
+        this.passRef.current.focus();
+
+        //this.name.focus()
+
+
+        }
+
+    }
 
     
     render() {
@@ -10,12 +33,17 @@ export default class Refs extends Component {
             <div>
 
 <div>
-                <labe>name:</labe>
-                <input ref={(input)=>this.name=input} type="text" name="name"/><br></br>
+                <label>name:</label>
+                <input  ref={(input)=>this.name=input} type="text"  onKeyDown={this.handleChange} name="name"/><br></br>
+
+
+                <input type="text" name="email"  ref={this.emailRef}/>
+
+                <input type="text" name="password"  ref={this.passRef}/>
 
                 </div>
-                <div><labe>Age:</labe>
-<input type="text" name="email"/>
+                <div><label>Age:</label>
+
 
                 </div>
             </div>
